09be4f7561487880f99226dec007668923731b23
Kabuda-czh <634469564@qq.com>
2023-01-15 02:13:54 +0800

    ✨ feat(config): 增加全局配置文本

---
+++ src/config/index.ts
@@ -0,0 +1,12 @@
+/*
+ * @Author: Kabuda-czh
+ * @Date: 2023-01-15 00:06:05
+ * @LastEditors: Kabuda-czh
+ * @LastEditTime: 2023-01-15 01:53:43
+ * @FilePath: \DDTV_WEBUI\src\config\index.ts
+ * @Description:
+ *
+ * Copyright (c) 2023 by Kabuda-czh, All Rights Reserved.
+ */
+export * from "./regexp";
+export * from "./service";
