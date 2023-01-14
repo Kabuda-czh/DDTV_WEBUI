4126e9e4a94c38c917d2f95c066237157aad5026
Kabuda-czh <634469564@qq.com>
2023-01-15 02:15:17 +0800

    ✨ feat(plugins): 增加静态文件的安装操作

---
+++ src/plugins/assets.ts
@@ -0,0 +1,18 @@
+/*
+ * @Author: Kabuda-czh
+ * @Date: 2023-01-14 23:19:49
+ * @LastEditors: Kabuda-czh
+ * @LastEditTime: 2023-01-15 01:06:07
+ * @FilePath: \DDTV_WEBUI\src\plugins\assets.ts
+ * @Description:
+ *
+ * Copyright (c) 2023 by Kabuda-czh, All Rights Reserved.
+ */
+import "element-plus/dist/index.css";
+import "nprogress/nprogress.css";
+import "tailwindcss.css";
+import "../styles/css/global.css";
+
+/** import static assets: css, js , font and so on. - [引入静态资源，css、js和字体文件等] */
+// eslint-disable-next-line @typescript-eslint/no-empty-function
+export default function setupAssets() {}
---
+++ src/plugins/index.ts
@@ -0,0 +1,13 @@
+/*
+ * @Author: Kabuda-czh
+ * @Date: 2023-01-14 23:19:44
+ * @LastEditors: Kabuda-czh
+ * @LastEditTime: 2023-01-14 23:27:17
+ * @FilePath: \DDTV_WEBUI\src\plugins\index.ts
+ * @Description:
+ *
+ * Copyright (c) 2023 by Kabuda-czh, All Rights Reserved.
+ */
+import setupAssets from "./assets";
+
+export { setupAssets };
