51ea5c7b710c5e3ac3dd2df1bac2b0b4aceedfff
Kabuda-czh <634469564@qq.com>
2023-01-15 03:47:13 +0800

    🐞 fix(assets.ts): 修复tailwindcss加载失败的问题

--- src/plugins/assets.ts
+++ src/plugins/assets.ts
@@ -2,7 +2,7 @@
  * @Author: Kabuda-czh
  * @Date: 2023-01-14 23:19:49
  * @LastEditors: Kabuda-czh
- * @LastEditTime: 2023-01-15 01:06:07
+ * @LastEditTime: 2023-01-15 03:04:13
  * @FilePath: \DDTV_WEBUI\src\plugins\assets.ts
  * @Description:
  *
@@ -10,7 +10,7 @@
  */
 import "element-plus/dist/index.css";
 import "nprogress/nprogress.css";
-import "tailwindcss.css";
+import "tailwindcss/tailwind.css";
 import "../styles/css/global.css";
 
 /** import static assets: css, js , font and so on. - [引入静态资源，css、js和字体文件等] */
