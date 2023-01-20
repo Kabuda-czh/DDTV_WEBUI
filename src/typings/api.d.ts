4d10dd3938fd8a695e3982dc8d1b256ebeeaac86
Kabuda-czh <634469564@qq.com>
2023-01-20 15:10:07 +0800

    🦄 refactor(typings): 对接后端的返回字段 以及api接口

--- src/typings/api.d.ts
+++ src/typings/api.d.ts
@@ -2,7 +2,7 @@
  * @Author: Kabuda-czh
  * @Date: 2023-01-15 00:11:42
  * @LastEditors: Kabuda-czh
- * @LastEditTime: 2023-01-15 00:11:48
+ * @LastEditTime: 2023-01-20 15:06:17
  * @FilePath: \DDTV_WEBUI\src\typings\api.d.ts
  * @Description:
  *
@@ -13,10 +13,42 @@
 /** 后端返回的用户权益相关类型 */
 declare namespace ApiAuth {
   /** 返回的token和刷新token */
-  interface Token {
-    token: string;
-    refreshToken: string;
-  }
+  // interface Token {
+  //   token: string;
+  //   refreshToken: string;
+  // }
+
   /** 返回的用户信息 */
-  type UserInfo = Auth.UserInfo;
+  // type UserInfo = Auth.UserInfo;
+  type Cookie = string;
+
+  interface SystemInfo {
+    DDTVCore_Ver: string;
+    Room_Quantity: number;
+    ServerName: string;
+    ServerAID: string;
+    os_Info: {
+      OS_Ver: string;
+      OS_Tpye: string;
+      Memory_Usage: number;
+      Runtime_Ver: string;
+      UserInteractive: boolean;
+      Associated_Users: string;
+      Current_Directory: string;
+      AppCore_Ver: string;
+      WebCore_Ver: string;
+    };
+    download_Info: {
+      Downloading: number;
+      Completed_Downloads: number;
+    };
+  }
+
+  interface SystemConfig {
+    Key: number;
+    KeyName: string;
+    Group: number;
+    Value: string;
+    Enabled: boolean;
+  }
 }
--- src/typings/system.d.ts
+++ src/typings/system.d.ts
@@ -2,7 +2,7 @@
  * @Author: Kabuda-czh
  * @Date: 2023-01-15 00:16:44
  * @LastEditors: Kabuda-czh
- * @LastEditTime: 2023-01-15 02:41:09
+ * @LastEditTime: 2023-01-20 14:16:29
  * @FilePath: \DDTV_WEBUI\src\typings\system.d.ts
  * @Description:
  *
@@ -52,13 +52,11 @@ declare namespace Service {
   /** 后端接口返回的数据结构配置 */
   interface BackendResultConfig {
     /** 表示后端请求状态码的属性字段 */
-    codeKey: string;
+    code: string;
     /** 表示后端请求数据的属性字段 */
-    dataKey: string;
+    cmd: string;
     /** 表示后端消息的属性字段 */
-    msgKey: string;
-    /** 后端业务上定义的成功请求的状态 */
-    successCode: number | string;
+    message: string;
   }
 
   /** 自定义的请求成功结果 */
